9326ddb5032b286929fbf540ae328441b97e79e7
MARIEMJR <122799110+MARIEMJR@users.noreply.github.com>
2024-02-28 15:21:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,51 +35,29 @@ def ReadBDD():
 
     return render_template('read_data.html', data=data)
 
+@app.route('/c_ajouter_client/', methods=['GET', 'POST'])
+def ajouter_client():
+    if request.method == 'POST':
+        # Récupérer les données du formulaire
+        nom = request.form['nom']
+        prenom = request.form['prenom']
+        email = request.form['email']
+        adresse = request.form['adresse']
+        phone = request.form['phone']
+        
+        # Insérer les données dans la base de données (ici, je suppose que tu as une table 'clients')
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        if conn is not None:
+            cursor.execute("INSERT INTO client (nom, prenom, adresse, phone, email) VALUES (?, ?, ?, ?, ?);", (nom, prenom, adresse, phone, email ))
+            conn.commit()
+            conn.close()
+            return redirect('/consultation/')
+        else:
+            return 'Erreur de connexion à la base de données'
 
-@app.route('/ajouter_client/', methods=['GET', 'POST'])                              
-                                                                                     
-def ajouter_client():                                                                
-                                                                                     
-    if request.method == 'POST':                                                     
-                                                                                     
-        # Récupérer les données du formulaire                                        
-                                                                                     
-        nom = request.form['nom']                                                    
-                                                                                     
-        prenom = request.form['prenom']                                              
-                                                                                     
-        email = request.form['email']                                                
-                                                                                     
-        adresse = request.form['adresse']                                            
-                                                                                     
-        phone = request.form['phone']                                                
-                                                                                     
-
-                                                                                     
-        # Insérer les données dans la base de données (ici, je suppose que tu as une table 'clients')                                                                     
-                                                                                     
-        conn = sqlite3.connect('database.db')                                        
-                                                                                     
-        cursor = conn.cursor()                                                       
-                                                                                     
-        if conn is not None:                                                         
-                                                                                     
-            cursor.execute("INSERT INTO client (nom, prenom, , email, adresse, phone) VALUES (?, ?, ?, ?, ?);", (nom, prenom,  email, adresse, phone, email ))            
-                                                                                     
-            conn.commit()                                                            
-                                                                                     
-            conn.close()                                                             
-                                                                                     
-        else:                                                                        
-                                                                                     
-            return 'Erreur de connexion à la base de données'                        
-                                                                                     
-                                                                                     
-        # Rediriger vers la page de consultation des clients après l'ajout           
-                                                                                     
-        return render_template('confirmation.html')                                  
-                                                                                     
-    return render_template('ajouter_client.html')                                    
+        
+    return render_template('ajouter_client.html')                                   
                                                                                      
 
                                                                                      
